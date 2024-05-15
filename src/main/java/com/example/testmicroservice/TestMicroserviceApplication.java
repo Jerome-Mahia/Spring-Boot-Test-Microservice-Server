af44b09686434570211ee4249446f96f70e72845
Jerome Mahia <jeromekamaumahia@gmail.com>
2024-05-16 00:20:59 +0300

    Second Commit

--- src/main/java/com/example/testmicroservice/TestMicroserviceApplication.java
+++ src/main/java/com/example/testmicroservice/TestMicroserviceApplication.java
@@ -1,4 +1,4 @@
-package com.example.ipslbackend;
+package com.example.testmicroservice;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
