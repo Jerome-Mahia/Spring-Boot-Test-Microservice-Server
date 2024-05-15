460fbaeacf6145d5ca0d5335e5b6c836da708cf5
Jerome Mahia <jeromekamaumahia@gmail.com>
2024-05-16 00:18:52 +0300

    first commit

---
+++ src/main/java/com/example/ipslbackend/TestMicroserviceApplication.java
@@ -0,0 +1,29 @@
+package com.example.ipslbackend;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+@RequestMapping("/test")
+public class TestMicroserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestMicroserviceApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    private String serverPort;
+
+
+    @GetMapping("/say")
+    public String getMethodName() {
+        return "Hi, I am from Microservice " + serverPort;
+    }
+}
---
+++ src/test/java/com/example/ipslbackend/IpslbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ipslbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpslbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
